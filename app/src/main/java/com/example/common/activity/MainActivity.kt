08ca70f704494b34afb0a2291c07b89c5bd860be
bogdanMerkulow <bogdanMerkulow@mail.ru>
2022-03-14 21:35:03 +0300

    Add view binding and change fragment to fragment container

--- app/src/main/java/com/example/common/activity/MainActivity.kt
+++ app/src/main/java/com/example/common/activity/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.common.activity
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.R
 import com.example.common.application.getApplicationComponent
+import com.example.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), ActivityComponentHolder {
+
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     private val component by lazy {
         getApplicationComponent()
@@ -16,7 +20,7 @@ class MainActivity : AppCompatActivity(), ActivityComponentHolder {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 
     override fun getActivityComponent(): ActivityComponent = component
